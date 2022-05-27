ececdcf6dff3ca68fc8dd3ecc92b4d4d6a8798b3
BlindDev-cloud <korsikoff@gmail.com>
2022-05-27 11:48:47 +0300

    Update task_08/index.php

--- task_08/index.php
+++ task_08/index.php
@@ -2,20 +2,13 @@
 
 $year = random_int(0, 9999);
 
-echo 'Year: '.$year.' ';
+echo 'Year: ' . $year . ' ';
 
-if(0 == $year % 4){
-    if(0 == $year % 100){
-        if(0 == $year % 400){
-            echo 'is a leap year.';
-        }else{
-            echo 'is not a leap year.';
-        }
-    }else{
-        echo 'is a leap year.';
-    }
-}else{
+if (0 == $year % 4 xor 0 == $year % 100 or 0 == $year % 400) {
+    echo 'is a leap year.';
+} else {
     echo 'is not a leap year.';
 }
 
 echo PHP_EOL;
+
